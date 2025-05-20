066151cfa9b58f84521a816dea08eb5298f4eb11
Veniamin Boiarkin <x86x64dev@gmail.com>
2025-05-20 16:04:53 +0100

    DAY 1

---
+++ DAY_1/main.py
@@ -0,0 +1,17 @@
+# Printing text
+print("1. Hello World!\n2. Hello World!\n3. Hello World!")
+
+# Strings concatenation
+print("4. Hello" + " " + "World" + " " + "!")
+
+# User input
+print("Hello, " + input("What is your name?") + "!")
+
+# Variables
+name = input("What is your real name?")
+print("Hello, " + name + "! You name is now stored in a variable =)")
+
+# Determining the length of the user input
+username = input("What is your name?")
+username_length = len(username)
+print("Hello, " + username + ". The length of your name is " + str(username_length))
